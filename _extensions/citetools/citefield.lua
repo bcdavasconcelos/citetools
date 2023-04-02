975427b95f02cd364fdad33916b51df340a4d403
Bernardo C.D.A. Vasconcelos <35749099+bcdavasconcelos@users.noreply.github.com>
2023-04-02 19:18:40 -0300

    2023-04-02-19-18

--- _extensions/citetools/citefield.lua
+++ _extensions/citetools/citefield.lua
@@ -3,9 +3,9 @@
 --- Copyright: © 2023 - Albert Krewinkel & Bernardo Vasconcelos
 --- License: MIT – see LICENSE for details
 
---- [@Citekey]{.csl_field} 
+--- [@Citekey]{.csl_field}
 --- to print any valid CSL field
---- [@Citekey]{.csl_field.} (with extra dot) 
+--- [@Citekey]{.csl_field.} (with extra dot)
 --- to do it without creating a link.
 
 --- ATTENTION: *MUST* *RUN* *AFTER* *CITEPROC*.
@@ -17,10 +17,10 @@ local stringify = require 'pandoc.utils'.stringify
 csl_fields = {"abstract", "accessed", "annote", "archive", "archive_collection", "archive_location", "archive-place", "author", "authority", "available-date", "call-number", "chair", "chapter-number", "citation-key", "citation-label", "citation-number", "collection-editor", "collection-number", "collection-title", "compiler", "composer", "container-author", "container-title", "container-title-short", "contributor", "curator", "dimensions", "director", "division", "DOI", "edition", "editor", "editor-translator", "editorial-director", "event", "event-date", "event-place", "event-title", "executive-producer", "first-reference-note-number", "genre", "guest", "host", "illustrator", "interviewer", "ISBN", "ISSN", "issue", "issued", "jurisdiction", "keyword", "language", "license", "locator", "medium", "narrator", "note", "number", "number-of-pages", "number-of-volumes", "organizer", "original-author", "original-date", "original-publisher", "original-publisher-place", "original-title", "page", "page-first", "part-number", "part-title", "performer", "PMCID", "PMID", "printing-number", "producer", "publisher", "publisher-place", "recipient", "references", "reviewed-author", "reviewed-genre", "reviewed-title", "scale", "script-writer", "section", "series-creator", "source", "status", "submitted", "supplement-number", "title", "title-short", "translator", "URL", "version", "volume", "volume-title", "year-suffix"}
 
 function get_options(meta)
-  if meta['link-citations'] or meta['link-fields'] then
-      return {link_citations = meta['link-citations'], link_fields = meta['link-fields']}
+  if meta['link-citations'] or meta['link-fields'] or meta['title-field-emphasis'] then
+      return {link_citations = meta['link-citations'], link_fields = meta['link-fields'], title_field_emph = meta['title-field-emphasis']}
   else
-      return {}        
+      return {}
   end
 end
 
@@ -37,11 +37,11 @@ function Pandoc (doc)
 
   doc.meta.references = pandoc.utils.references(doc)
   doc.meta.bibliography = nil
-  
+
   return doc:walk{
-  
+
     Span = function (span)
-  
+
       -- check that the span contains only a single cite object
       local cite = span.content[1]
       local citations = cite and cite.citations or nil
@@ -90,6 +90,7 @@ function Pandoc (doc)
 
       local the_result = "" -- initialize result
       local content = ref[the_arg] -- get field
+      local title_field_emph = get_options(doc.meta).title_field_emph
 
       if content then -- if field is not empty
         if the_arg == "author" or the_arg == "editor" or the_arg == "translator" then -- if field contains name
@@ -97,7 +98,7 @@ function Pandoc (doc)
             if content[ordinal]["family"] then -- if name[ord] contains family name
               the_result = ref[the_arg][ordinal]["family"] -- get family name
               else if ref[the_arg][ordinal]["literal"] then -- if name[ord] does not contain family name, but contains literal
-                the_result = ref[the_arg][ordinal]["literal"] 
+                the_result = ref[the_arg][ordinal]["literal"]
               else -- if field does not contain family name or literal
                 the_result = "#ERROR#"
               end
@@ -106,9 +107,9 @@ function Pandoc (doc)
             print("ERROR: [".. cite_id .. "][" .. the_arg .. "][" .. ordinal .. "] is empty")
             return "#ERROR: [".. cite_id .. "][" .. the_arg .. "][" .. ordinal .. "] is empty#"
           end -- end of field contains name
-        else if the_arg == "title" then -- if field is a title
+        else if the_arg == "title" and title_field_emph ~= false then -- if field is a title
             the_result = pandoc.Emph{stringify(ref[the_arg])}
-          else if the_arg == "title-short" then -- if field is a short title
+          else if the_arg == "title-short" and title_field_emph ~= false then -- if field is a short title
               the_result = pandoc.Emph{stringify(ref[the_arg])}
             else -- if field is not a name or a title
               the_result = stringify(ref[the_arg])
@@ -116,7 +117,7 @@ function Pandoc (doc)
           end -- end of field is a short title
         end -- end of field is a title
       else -- end of field is empty
-        the_result = span 
+        the_result = span
       end
 
       if dotted == true
@@ -126,7 +127,7 @@ function Pandoc (doc)
       or the_arg == "annote"
       or get_options(doc.meta).link_citations == false
       then -- if field is not to be linked
-        return the_result     
+        return the_result
       else -- if field is to be linked
         if get_options(doc.meta).link_citations == true and get_options(doc.meta).link_fields ~= false then
           the_result = pandoc.Link(the_result, "#ref-"..cite_id)
@@ -138,9 +139,9 @@ function Pandoc (doc)
       end -- end of field is to be linked
 
     end -- end of span contains only a single cite object
-  
+
   end -- end of Span
-  
+
   } -- end of doc:walk
 
 end -- end of Pandoc
--- changelog.md
+++ changelog.md
@@ -1,6 +1,12 @@
 citetools
 ========
 
+v1.0.1
+------
+
+Released 2023-04-02.
+- Added `title-field-emphasis` option (default: `true`).
+
 v1.0.0
 ------
 
